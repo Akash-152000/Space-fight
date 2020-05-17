99b5fd4bcdde8280bb2c17157c0900305670692f
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-05-17 11:33:49 +0530

    Update game1.py

--- game1.py
+++ game1.py
@@ -5,44 +5,61 @@ gameExit=True
 surface=pygame.display.set_mode((800,600))
 pygame.display.set_caption("Game1")
 x=0
-y=500
+y=555
 height=50
 width=50
 white=(255,255,255)
-
+clock=pygame.time.Clock()
 ball=pygame.image.load("ball.png")
-ax=random.randrange(10,790)
-ay=900
-height_a=20
+ax=800
+ay=580
+height_a=2100
 width_a=60
 speed=7
-
+jumpCount=10
+isJump=False
+num=2
 
 while gameExit:
-    ax=random.randrange(10,790)
-    ay=900
-    height_a=20
-    width_a=60
-    speed=7
+    if num<30:
+        num+=0.01
+#    pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             gameExit=False
         key=pygame.key.get_pressed()
     if key[pygame.K_RIGHT] and x<=790-width:
-        x+=1
+        x+=5
     if key[pygame.K_LEFT] and x>=10:
-        x-=1
+        x-=5
+
+    if not(isJump):
+        if key[pygame.K_SPACE]:
+            isJump=True
+    else:
+        if jumpCount>=-10:
+            y-=(jumpCount*abs(jumpCount))*0.5
+            jumpCount-=1
+        else:
+            jumpCount=10
+            isJump=False 
+
     
     surface.fill((255,255,255))
 
     pygame.draw.rect(surface,(0,0,255),(ax,ay,width_a,height_a))
-    ay-=1
+    ax-=num
     
-    if ay<10:
-        ax=random.randrange(10,790)
-        ay=height_a
+    if ax<-100:
+        ax=800
+        ay=random.randrange(450,580)
+
+
+
     surface.blit(ball,(x,y))
     
     
     pygame.display.flip()
+    clock.tick(60)
 pygame.quit()
+
